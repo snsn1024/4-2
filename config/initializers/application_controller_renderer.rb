d0added4c7f5694048fd22a8aef216001a0a98c4
snsn1024 <47966782+snsn1024@users.noreply.github.com>
2019-03-21 15:19:10 +0900

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'bd2460ce122040ebc6e4e62634d9408f2de7e1e7f488b44ad9c7b7fc9fc22d7f0c74a7cdd7bc8a647aec2b5d1a0a91a4ba344d7aa6ba38afb1eaf3fbc0c84176'
+
